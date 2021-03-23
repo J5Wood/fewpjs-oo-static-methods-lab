5aabb19392c0ad5259925261cc1881b8e03e3abb
Jeremy Wood <js.wood206@gmail.com>
2021-03-22 18:11:40 -0700

    commit tree 15b3d445a1b7bb89d54fe5357f0925990889b8e5

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+  
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string) {
+    const offLimitWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const first = Formatter.capitalize(string).split(" ").slice(0,1)
+    const split = string.split(" ").slice(1)
+    for (const key in split) {
+      if (!offLimitWords.includes(split[key])) {
+        split[key] = Formatter.capitalize(split[key]);
+      }
+    }
+    return first + " " + split.join(" ")
+  }
 }
